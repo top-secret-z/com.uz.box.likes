5b18c2741447ab64478ffba0bcdc32c8ca8a165e
Sascha Greuel <github@1-2.dev>
2022-09-18 22:35:07 +0200

    5.4.2

--- files/lib/system/box/UzBoxLikesBoxController.class.php
+++ files/lib/system/box/UzBoxLikesBoxController.class.php
@@ -1,110 +1,136 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\box;
+
 use wcf\system\cache\builder\UzBoxLikesCacheBuilder;
-use wcf\system\box\AbstractDatabaseObjectListBoxController;
 use wcf\system\event\EventHandler;
 use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
 
 /**
  * Most likes box controller.
- *
- * @author		2018-2022 Zaydowicz.de
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.box.likes
  */
-class UzBoxLikesBoxController extends AbstractDatabaseObjectListBoxController {
-	/**
-	 * @inheritDoc
-	 */
-	protected $conditionDefinition = 'com.uz.box.likes.condition';
-	
-	/**
-	 * @inheritDoc
-	 */
-	public $defaultLimit = 5;
-	public $maximumLimit = 100;
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $sortFieldLanguageItemPrefix = 'com.uz.box.likes';
-	
-	/**
-	 * data
-	 */
-	protected $lasts = [];
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function getLink() {
-		if (MODULE_MEMBERS_LIST) {
-			$parameters = 'sortField=likesReceived&sortOrder=DESC';
-			
-			return LinkHandler::getInstance()->getLink('MembersList', [], $parameters);
-		}
-		
-		return '';
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getObjectList() {
-		// get conditions as parameters for cache builder
-		$parameters = [];
-		foreach ($this->box->getConditions() as $condition) {
-			$parameters[] = $condition->conditionData;
-		}
-		$parameters[] = ['limit' => $this->limit];
-		
-		$temp = UzBoxLikesCacheBuilder::getInstance()->getData($parameters);
-		$userList = $temp['users'];
-		$this->lasts = $temp['lasts'];
-		
-		return $userList;
-	
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getTemplate() {
-		return WCF::getTPL()->fetch('boxUzLikes', 'wcf', [
-				'boxUserList' => $this->objectList,
-				'lasts' => $this->lasts
-		], true);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function hasContent() {
-		// module
-		if (!MODULE_LIKE) return false;
-		
-		// object list
-		if ($this->objectList === null) {
-			$this->objectList = $this->getObjectList();
-		}
-		
-		EventHandler::getInstance()->fireAction($this, 'hasContent');
-		
-		return ($this->objectList !== null && count($this->objectList) > 0);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function loadContent() {
-		$this->content = $this->getTemplate();
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function hasLink() {
-		return MODULE_MEMBERS_LIST == 1;
-	}
+class UzBoxLikesBoxController extends AbstractDatabaseObjectListBoxController
+{
+    /**
+     * @inheritDoc
+     */
+    protected $conditionDefinition = 'com.uz.box.likes.condition';
+
+    /**
+     * @inheritDoc
+     */
+    public $defaultLimit = 5;
+
+    public $maximumLimit = 100;
+
+    /**
+     * @inheritDoc
+     */
+    protected $sortFieldLanguageItemPrefix = 'com.uz.box.likes';
+
+    /**
+     * data
+     */
+    protected $lasts = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function getLink()
+    {
+        if (MODULE_MEMBERS_LIST) {
+            $parameters = 'sortField=likesReceived&sortOrder=DESC';
+
+            return LinkHandler::getInstance()->getLink('MembersList', [], $parameters);
+        }
+
+        return '';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getObjectList()
+    {
+        // get conditions as parameters for cache builder
+        $parameters = [];
+        foreach ($this->box->getConditions() as $condition) {
+            $parameters[] = $condition->conditionData;
+        }
+        $parameters[] = ['limit' => $this->limit];
+
+        $temp = UzBoxLikesCacheBuilder::getInstance()->getData($parameters);
+        $userList = $temp['users'];
+        $this->lasts = $temp['lasts'];
+
+        return $userList;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getTemplate()
+    {
+        return WCF::getTPL()->fetch('boxUzLikes', 'wcf', [
+            'boxUserList' => $this->objectList,
+            'lasts' => $this->lasts,
+        ], true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasContent()
+    {
+        // module
+        if (!MODULE_LIKE) {
+            return false;
+        }
+
+        // object list
+        if ($this->objectList === null) {
+            $this->objectList = $this->getObjectList();
+        }
+
+        EventHandler::getInstance()->fireAction($this, 'hasContent');
+
+        return $this->objectList !== null && \count($this->objectList) > 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function loadContent()
+    {
+        $this->content = $this->getTemplate();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hasLink()
+    {
+        return MODULE_MEMBERS_LIST == 1;
+    }
 }
--- files/lib/system/condition/UzBoxLikesLastCondition.class.php
+++ files/lib/system/condition/UzBoxLikesLastCondition.class.php
@@ -1,60 +1,83 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\condition;
-use wcf\data\user\UserList;
+
+use InvalidArgumentException;
 use wcf\data\DatabaseObjectList;
-use wcf\system\condition\AbstractTextCondition;
-use wcf\system\condition\IObjectListCondition;
-use wcf\system\WCF;
+use wcf\data\user\UserList;
 
 /**
  * Condition implementation for a last period value.
- * 
- * @author		2018-2022 Zaydowicz.de
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.box.likes
  */
-class UzBoxLikesLastCondition extends AbstractTextCondition implements IObjectListCondition {
-	/**
-	 * @inheritDoc
-	 */
-	protected $description = 'wcf.acp.box.uzlikes.condition.last.description';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $fieldName = 'uzboxLikesLast';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $fieldValue = '1';
-	/**
-	 * @inheritDoc
-	 */
-	protected $label = 'wcf.acp.box.uzlikes.condition.last';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getFieldElement() {
-		return '<input type="number" name="'.$this->fieldName.'" value="'.$this->fieldValue.'" class="tiny" min="0">';
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function readFormParameters() {
-		if (isset($_POST[$this->fieldName])) $this->fieldValue = intval($_POST[$this->fieldName]);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData) {
-		if (!($objectList instanceof UserList)) {
-			throw new \InvalidArgumentException("Object list is no instance of '".UserList::class."', instance of '".get_class($objectList)."' given.");
-		}
-		
-		// do nothing
-	}
+class UzBoxLikesLastCondition extends AbstractTextCondition implements IObjectListCondition
+{
+    /**
+     * @inheritDoc
+     */
+    protected $description = 'wcf.acp.box.uzlikes.condition.last.description';
+
+    /**
+     * @inheritDoc
+     */
+    protected $fieldName = 'uzboxLikesLast';
+
+    /**
+     * @inheritDoc
+     */
+    protected $fieldValue = '1';
+
+    /**
+     * @inheritDoc
+     */
+    protected $label = 'wcf.acp.box.uzlikes.condition.last';
+
+    /**
+     * @inheritDoc
+     */
+    protected function getFieldElement()
+    {
+        return '<input type="number" name="' . $this->fieldName . '" value="' . $this->fieldValue . '" class="tiny" min="0">';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function readFormParameters()
+    {
+        if (isset($_POST[$this->fieldName])) {
+            $this->fieldValue = \intval($_POST[$this->fieldName]);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData)
+    {
+        if (!($objectList instanceof UserList)) {
+            throw new InvalidArgumentException("Object list is no instance of '" . UserList::class . "', instance of '" . \get_class($objectList) . "' given.");
+        }
+
+        // do nothing
+    }
 }
